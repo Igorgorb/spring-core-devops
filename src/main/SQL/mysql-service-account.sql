03c534b6894df164ffe40981cf5ca37a4fdf3f8e
Ihor Horbonos <igorgorb@gmail.com>
2025-02-18 15:32:04 +0200

    create and run second qa-two environment

--- src/main/SQL/mysql-service-account.sql
+++ src/main/SQL/mysql-service-account.sql
@@ -1,6 +1,15 @@
+use mydatabase;
 CREATE USER 'springframework'@'%' IDENTIFIED BY 'guru';
 
 GRANT SELECT ON mydatabase.* to 'springframework'@'%';
 GRANT INSERT ON mydatabase.* to 'springframework'@'%';
 GRANT DELETE ON mydatabase.* to 'springframework'@'%';
 GRANT UPDATE ON mydatabase.* to 'springframework'@'%';
+
+use qatwo;
+CREATE USER 'uqatwo'@'%' IDENTIFIED BY 'qatwo-guru';
+
+GRANT SELECT ON qatwo.* to 'uqatwo'@'%';
+GRANT INSERT ON qatwo.* to 'uqatwo'@'%';
+GRANT DELETE ON qatwo.* to 'uqatwo'@'%';
+GRANT UPDATE ON qatwo.* to 'uqatwo'@'%';
